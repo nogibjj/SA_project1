4bf4d31206c143035910bd929583fee6fbc60e56
Susanna Anil <55010363+sanil72900@users.noreply.github.com>
2022-09-18 17:04:48 +0000

    add cli ex

--- dblib/querydb.py
+++ dblib/querydb.py
@@ -1,8 +1,8 @@
-from databricks import sql
 import os
+from databricks import sql
 
 
-def querydb(query="SELECT * FROM default.diamonds LIMIT 2"):
+def querydb(query="SELECT * FROM world_pop LIMIT 2"):
     with sql.connect(
         server_hostname=os.getenv("DATABRICKS_SERVER_HOSTNAME"),
         http_path=os.getenv("DATABRICKS_HTTP_PATH"),
